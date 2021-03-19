589ac7044c64efd8157aa5fb59d753613c34bc40
Daniel <danielanugr@yahoo.co.id>
2021-03-19 21:09:31 +0700

    Controller for auth + band done

---
+++ controllers/bandController.js
@@ -0,0 +1,11 @@
+const { Band } = require("../models");
+
+module.exports = class BandController {
+  static findAll(req, res) {}
+
+  static find(req, res) {}
+
+  static createProfile(req, res) {}
+
+  static updateProfile(req, res) {}
+};
