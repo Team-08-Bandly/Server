8cb87a6ef6ae4cf712508c553082309ca0b92bba
dzunnurainachmad <dzunnurainaaz@gmail.com>
2021-03-20 16:12:43 +0700

    fix asosiasi + create controller

--- models/genre.js
+++ models/genre.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Genre.belongsToMany(models.Band, { through: models.BandGenre })
     }
   };
   Genre.init({
