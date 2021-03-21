f3812e689b439639f9b97704bdc821dc606c95f8
Burhan Yusuf <burhanyusuf@outlook.com>
2021-03-21 14:42:25 +0700

    Transaction

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const bandRouter = require("./bandRouter");
 const userRouter = require("./userRouter");
+const transactionRouter = require("./transactionRouter");
 const AuthController = require("../controllers/authController");
 
 router.post("/login", AuthController.login);
 router.post("/register", AuthController.register);
 router.use("/users", userRouter);
 router.use("/bands", bandRouter);
+router.use('/transactions',transactionRouter)
 
 module.exports = router;
